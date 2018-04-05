18a2845d5a58503510726b80045fd5a1599035a5
RyanHD <ryanhd@uci.edu>
2018-04-05 16:58:53 -0700

    Added Time Sneing

--- Kevin_Conductivity.ino
+++ Kevin_Conductivity.ino
@@ -1,4 +1,4 @@
-// # 
+// #
 // # Editor     : YouYou from DFRobot
 // # Date       : 23.04.2014
 // # E-Mail  : youyou.yu@dfrobot.com
@@ -28,9 +28,11 @@ unsigned int readings[numReadings];      // the readings from the analog input
 byte index = 0;                  // the index of the current reading
 unsigned long AnalogValueTotal = 0;                  // the running total
 unsigned int AnalogAverage = 0,averageVoltage=0;                // the average
-unsigned long AnalogSampleTime,printTime,tempSampleTime;
-float temperature,ECcurrent; 
- 
+unsigned long AnalogSampleTime,printTime,tempSampleTime, time, timeInject, timeRun, timeFinal;
+float temperature,ECcurrent;
+boolean inject;
+static boolean stop = false;
+
 //Temperature chip i/o
 OneWire ds(DS18B20_Pin);  // on digital pin 2
 
@@ -50,7 +52,7 @@ void loop() {
   /*
    Every once in a while,sample the analog value and calculate the average.
   */
-  if(millis()-AnalogSampleTime>=AnalogSampleInterval)  
+  if(millis()-AnalogSampleTime>=AnalogSampleInterval)
   {
     AnalogSampleTime=millis();
      // subtract the last reading:
@@ -72,10 +74,11 @@ void loop() {
    Every once in a while,MCU read the temperature from the DS18B20 and then let the DS18B20 start the convert.
    Attention:The interval between start the convert and read the temperature should be greater than 750 millisecond,or the temperature is not accurate!
   */
-   if(millis()-tempSampleTime>=tempSampleInterval) 
+   if(millis()-tempSampleTime>=tempSampleInterval)
   {
     tempSampleTime=millis();
-    temperature = TempProcess(ReadTemperature);  // read the current temperature from the  DS18B20
+    //temperature = TempProcess(ReadTemperature);  // read the current temperature from the  DS18B20
+    temperature = 25f;
     TempProcess(StartConvert);                   //after the reading,start the convert for next reading
   }
    /*
@@ -93,22 +96,36 @@ void loop() {
     Serial.print("temp:");
     Serial.print(temperature);    //current temperature
     Serial.print("^C     EC:");
-    
+
     float TempCoefficient=1.0+0.0185*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.0185*(fTP-25.0));
-    float CoefficientVolatge=(float)averageVoltage/TempCoefficient;   
+    float CoefficientVolatge=(float)averageVoltage/TempCoefficient;
     if(CoefficientVolatge<150)Serial.println("No solution!");   //25^C 1413us/cm<-->about 216mv  if the voltage(compensate)<150,that is <1ms/cm,out of the range
     else if(CoefficientVolatge>3300)Serial.println("Out of the range!");  //>20ms/cm,out of the range
     else
-    { 
+    {
       if(CoefficientVolatge<=448)ECcurrent=6.84*CoefficientVolatge-64.32;   //1ms/cm<EC<=3ms/cm
       else if(CoefficientVolatge<=1457)ECcurrent=6.98*CoefficientVolatge-127;  //3ms/cm<EC<=10ms/cm
       else ECcurrent=5.3*CoefficientVolatge+2278;                           //10ms/cm<EC<20ms/cm
       ECcurrent/=1000;    //convert us/cm to ms/cm
-      Serial.print(ECcurrent,2);  //two decimal
+      Serial.print(ECcurrent,3);  //two decimal
       Serial.println("ms/cm");
     }
   }
-
+  if(ECcurrent > 3300){
+    inject = false;
+  }else{
+    timeInject = millis();
+    inject = true;
+  }
+  timeRun = millis();
+  timeRun = timeRun - timeInject;
+  timeRun = timeRun / 1000.0;
+  if(inject && ECcurrent < 8 && !stop){
+    timeFinal = millis;
+    timeFinal = timeFinal / 1000.0;
+    stop = true;
+  }
+  Serial.print("        Run Time: ");Serial.print(timeRun);Serial.print("         Inject Time: "); Serial.print(timeInject); Serial.print("       Final Time: ");Serial.print(timeFinal);
 }
 /*
 ch=0,let the DS18B20 start the convert;ch=1,MCU read the current temperature from the DS18B20.
@@ -124,31 +141,31 @@ float TempProcess(bool ch)
               //Serial.println("no more sensors on chain, reset search!");
               ds.reset_search();
               return 0;
-          }      
+          }
           if ( OneWire::crc8( addr, 7) != addr[7]) {
               Serial.println("CRC is not valid!");
               return 0;
-          }        
+          }
           if ( addr[0] != 0x10 && addr[0] != 0x28) {
               Serial.print("Device is not recognized!");
               return 0;
-          }      
+          }
           ds.reset();
           ds.select(addr);
           ds.write(0x44,1); // start conversion, with parasite power on at the end
   }
-  else{  
+  else{
           byte present = ds.reset();
-          ds.select(addr);    
-          ds.write(0xBE); // Read Scratchpad            
+          ds.select(addr);
+          ds.write(0xBE); // Read Scratchpad
           for (int i = 0; i < 9; i++) { // we need 9 bytes
             data[i] = ds.read();
-          }         
-          ds.reset_search();           
+          }
+          ds.reset_search();
           byte MSB = data[1];
-          byte LSB = data[0];        
+          byte LSB = data[0];
           float tempRead = ((MSB << 8) | LSB); //using two's compliment
           TemperatureSum = tempRead / 16;
     }
-          return TemperatureSum;  
+          return TemperatureSum;
 }
